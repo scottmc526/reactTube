8bee6711576bdc1931858afa0f426db9023c55c4
Scott Christensen <scottmc526@gmail.com>
2016-12-21 12:49:24 -0700

    refactor app to be a class based component, put youtube api callout inside class

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 //used to create and manage components
-import React from 'react';
+import React, { Component } from 'react';
 //used to interact with DOM
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyA3rJ41uVetgnXr50rOb8G26i1TM8or46E';
 
-YTSearch({key: API_KEY, term: 'surfing'}, function(data) {
-  console.log(data);
-})
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    //if you have key and value with same string you can refactor to just the key ie {videos: videos} becomes {videos}
+    YTSearch({key: API_KEY, term: 'surfing'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
