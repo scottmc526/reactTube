25ac6e9867d7867331009237fd6890b84fefcc8c
Scott Christensen <scottmc526@gmail.com>
2016-12-22 12:48:01 -0700

    video detail component renders, problem with iframe

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+  if(!video) {
+    return <div>Loading...</div>
+  }
+
   const title = video.snippet.title;
   const description = video.snippet.description;
   const videoId = video.id.videoId;
   //string interpolation
-  const url = `https://www.youtube.com/embed/${videoId}`
+  const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className='video-detail col-md-8'>
       <div className='embed-responsive embed-responsive16by9'>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyA3rJ41uVetgnXr50rOb8G26i1TM8or46E';
 
 
@@ -15,7 +16,7 @@ class App extends Component {
     this.state = { videos: [] };
 
     //if you have key and value with same string you can refactor to just the key ie {videos: videos} becomes {videos}
-    YTSearch({key: API_KEY, term: 'surfing'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'bowling'}, (videos) => {
       this.setState({ videos });
     })
   }
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
